18c6221cca97ff99093bf6c28dd3aa6a83d043e0
CrumrineCoder <crumrinecoding@gmail.com>
2019-01-07 09:50:05 -0500

    Profile Page redesign
    *Still need to implement routing to the back end
    *Still need to implement getting the already existing variable into the popups
    *Still need to implement the change password one
    *Still need to add an opague black screen while the popup is active that when clicked on will also close the popup

--- src/component/management/profile/ProfileView.jsx
+++ src/component/management/profile/ProfileView.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 
 import LoadingScreen from '../../common/loading-screen/LoadingScreen';
 import ErrorMessageComponent from '../../common/error-message-component/ErrorMessageComponent';
@@ -6,6 +6,7 @@ import NavContainer from '../../common/nav-container/NavContainer';
 import LeftNav from '../../common/left-nav/LeftNav';
 import userApi from '../../../api/userApi';
 import tenantApi from '../../../api/tenantApi';
+import ProfilePopup from "./profile-popups/ProfilePopup.jsx";
 
 import * as AppConstants from '../../../constants/AppConstants';
 import * as Config from '../../../config/config';
@@ -27,7 +28,7 @@ class ProfileView extends React.Component {
         this.dropDownClick = this.dropDownClick.bind(this);
 
         // Setting initial state objects
-        this.state  = this.getInitialState();
+        this.state = this.getInitialState();
     }
 
     componentDidMount() {
@@ -40,14 +41,14 @@ class ProfileView extends React.Component {
 
         if (siteLoginCookie) {
             var loggedUserObject = JSON.parse(siteLoginCookie);
-            this.setState({loggedUserObj: loggedUserObject});
+            this.setState({ loggedUserObj: loggedUserObject });
 
             this.getLoggedUserData(loggedUserObject);
         } else {
             UIHelper.redirectTo(AppConstants.LOGIN_ROUTE);
         }
 
-        this.setState({isLeftNavCollapse: UIHelper.getLeftState()});
+        this.setState({ isLeftNavCollapse: UIHelper.getLeftState() });
     }
 
     // Returns initial props
@@ -57,18 +58,27 @@ class ProfileView extends React.Component {
             loadingMessage: '',
             loggedUserObj: null,
             isLeftNavCollapse: false,
+            isHidden: true,
+            selectedPopup: "",
             tenantList: [],
             selectedTenant: {
-                email: {value: '', error: {}},
-                password: {value: '', error: {}},
-                name: {value: '', error: {}}
+                email: { value: '', error: {} },
+                password: { value: '', error: {} },
+                name: { value: '', error: {} }
             },
             profileObj: {
-                emailPassword: {value: '', error: {}}
+                emailPassword: { value: '', error: {} }
             }
         };
 
         return initialState;
+    }
+
+    togglePopup(id) {
+        this.setState({
+            isHidden: !this.state.isHidden,
+            selectedPopup: id
+        })
     }
 
     getLoggedUserData(loggedUserObj) {
@@ -77,20 +87,20 @@ class ProfileView extends React.Component {
 
     getAllTenantsData(user, context) {
         var urlToGetTenantData = Config.API_URL + AppConstants.GET_TENANT_DATA_API;
-        context.setState({isLoading: true, loadingMessage: MessageConstants.FETCHING_TENANTS});
-        tenantApi.getAllTenantsFrom(urlToGetTenantData, {userID: user._id}).then((data) => {
+        context.setState({ isLoading: true, loadingMessage: MessageConstants.FETCHING_TENANTS });
+        tenantApi.getAllTenantsFrom(urlToGetTenantData, { userID: user._id }).then((data) => {
 
             var tenantList = [];
 
             for (var i = 0; i < data.length; i++) {
                 var tenant = data[i];
-                tenant.email = {value: data[i].email, error: {}};
-                tenant.password = {value: '', error: {}};
-                tenant.name = {value: data[i].name, error: {}};
+                tenant.email = { value: data[i].email, error: {} };
+                tenant.password = { value: '', error: {} };
+                tenant.name = { value: data[i].name, error: {} };
                 tenantList.push(tenant);
             }
 
-            context.setState (
+            context.setState(
                 {
                     isLoading: false,
                     loadingMessage: '',
@@ -110,8 +120,8 @@ class ProfileView extends React.Component {
     updateMailClick(e) {
         e.preventDefault();
 
-        this.setState({isLoading: true, loadingMessage: MessageConstants.UPDATE_PROFILE});
-        var {profileObj, loggedUserObj} = this.state;
+        this.setState({ isLoading: true, loadingMessage: MessageConstants.UPDATE_PROFILE });
+        var { profileObj, loggedUserObj } = this.state;
 
         var emailSettingToInsert = {
             id: loggedUserObj._id,
@@ -135,7 +145,7 @@ class ProfileView extends React.Component {
     }
 
     leftNavStateUpdate() {
-        this.setState({isLeftNavCollapse: !this.state.isLeftNavCollapse});
+        this.setState({ isLeftNavCollapse: !this.state.isLeftNavCollapse });
     }
 
     dropDownClick(stateObject) {
@@ -150,105 +160,163 @@ class ProfileView extends React.Component {
             isLeftNavCollapse,
             tenantList,
             selectedTenant,
-            profileObj
+            profileObj,
+            selectedPopup
         } = this.state;
-
+   
+        /*
+                <div className="col-sm-9">
+                    <div className="form-group">
+                        <input
+                            value={profileObj.emailPassword.value}
+                            onChange={(e) => {
+                                profileObj.emailPassword = {
+                                    value: e.target.value
+                                }
+                                this.handleChange(e, { profileObj });
+                            }}
+                            type="password"
+                            className="form-control"
+                            id="passwordInput"
+                            placeholder="PASSWORD" />
+                    </div>
+                </div>
+        */
         return (
             <Fragment>
-                <LoadingScreen isDisplay={isLoading} message={loadingMessage}/>
+                <LoadingScreen isDisplay={isLoading} message={loadingMessage} />
                 <LeftNav
                     selectedIndex={AppConstants.USER_PROFILE_INDEX}
                     isFixedLeftNav={true}
                     leftNavStateUpdate={this.leftNavStateUpdate}
-                    isSubSectionExpand={true}/>
+                    isSubSectionExpand={true} />
                 {
                     (loggedUserObj)
                         ? <NavContainer
-                              loggedUserObj={loggedUserObj}
-                              isFixedNav={true}/>
+                            loggedUserObj={loggedUserObj}
+                            isFixedNav={true} />
                         : <div className="sign-in-button">
-                              <button onClick={() => {UIHelper.redirectTo(AppConstants.LOGIN_ROUTE);}}
-                                  className="btn btn-primary btn-sm log-out-drop-down--li--button">
-                                  Sign in
+                            <button onClick={() => { UIHelper.redirectTo(AppConstants.LOGIN_ROUTE); }}
+                                className="btn btn-primary btn-sm log-out-drop-down--li--button">
+                                Sign in
                               </button>
-                          </div>
+                        </div>
                 }
+
+                {!this.state.isHidden &&  <ProfilePopup
+            selectedPopup={selectedPopup}
+            closePopup={this.togglePopup.bind(this)}
+          />}
                 <div className="site-edit-container">
-                    <div className = {
+                    <div className={
                         'table-container-div ' +
                         ((isLeftNavCollapse) ? 'collapse-left-navigation' : 'expand-left-navigation')}>
-                        <div className="row alert-list-wrap-div">
-                            <h1 className="site-add-title">
-                                Profile
-                            </h1>
-                        </div>
 
                         <div className="row alert-list-wrap-div settings-section">
                             <div className="row">
                                 <div className="col-sm-3 alert-label-column section-head">
                                     <h4 className="site-add-title">
-                                        Email Settings
+                                        Profile Settings
                                     </h4>
                                 </div>
                             </div>
-                            <div className="row">
+                            <div className="row profile-row" onClick={this.togglePopup.bind(this, "Name")}>
                                 <div className="col-sm-3 alert-label-column">
                                     <div className="form-group label-text">
-                                        <label className="control-label">Account's Email</label>
+                                        <label className="control-label">Name</label>
+                                    </div>
+                                </div>
+                                <div className="col-sm-9" >
+                                    <p className="profile-label">
+                                        Jane Smith
+                                    </p>
+                                    <i class="fas fa-edit edit-icon"></i>
+                                </div>
+                            </div>
+                            <div className="row profile-row" onClick={this.togglePopup.bind(this, "Email")}>
+                                <div className="col-sm-3 alert-label-column">
+                                    <div className="form-group label-text">
+                                        <label className="control-label">Email</label>
                                     </div>
                                 </div>
                                 <div className="col-sm-9">
-                                    <div className="form-group has-feedback label-div">
-                                        <label className="alert-label">
-                                            {loggedUserObj.email}
-                                        </label>
-                                    </div>
+                                    <p className="profile-label">
+                                        {loggedUserObj.email}
+                                    </p>
+                                    <i class="fas fa-edit edit-icon"></i>
                                 </div>
                             </div>
-
-                            <div className="row">
+                            <div className="row profile-row" onClick={this.togglePopup.bind(this, "Company")}>
                                 <div className="col-sm-3 alert-label-column">
                                     <div className="form-group label-text">
-                                        <label className="control-label">Email Password</label>
+                                        <label className="control-label">Company</label>
                                     </div>
                                 </div>
                                 <div className="col-sm-9">
-                                    <div className="form-group">
-                                        <input
-                                            value={profileObj.emailPassword.value}
-                                            onChange={(e) => {
-                                                profileObj.emailPassword = {
-                                                    value: e.target.value
-                                                }
-                                                this.handleChange(e, {profileObj});
-                                            }}
-                                            type="password"
-                                            className="form-control"
-                                            id="passwordInput"
-                                            placeholder="Password"/>
-                                    </div>
+                                    <p className="profile-label">
+                                        xOps
+                                    </p>
+                                    <i class="fas fa-edit edit-icon"></i>
                                 </div>
                             </div>
-                        </div>
-
-                        <div className="row alert-list-wrap-div">
-                            {
-                                (loggedUserObj.permissions && loggedUserObj.permissions.canUpdate)
-                                    ? <div className="row">
-                                        <div className="col-sm-4 alert-label-column">
-                                        </div>
-                                        <div className="col-sm-3 alert-label-column">
-                                            <div className="form-group">
-                                                <button
-                                                    className="btn btn-primary form-control button-all-caps-text"
-                                                    onClick={(e) => this.updateMailClick(e)}>
-                                                    Save Settings
+                            <div className="row profile-row" onClick={this.togglePopup.bind(this, "Title")}>
+                                <div className="col-sm-3 alert-label-column">
+                                    <div className="form-group label-text">
+                                        <label className="control-label">Title</label>
+                                    </div>
+                                </div>
+                                <div className="col-sm-9">
+                                    <p className="profile-label">
+                                        Developer
+                                     </p>
+                                     <i class="fas fa-edit edit-icon"></i>
+                                </div>
+                            </div>
+                            <div className="row profile-row" onClick={this.togglePopup.bind(this, "Location")}>
+                                <div className="col-sm-3 alert-label-column">
+                                    <div className="form-group label-text">
+                                        <label className="control-label">Location</label>
+                                    </div>
+                                </div>
+                                <div className="col-sm-9">
+                                    <p className="profile-label">
+                                        Earth
+                                    </p>
+                                    <i class="fas fa-edit edit-icon"></i>
+                                </div>
+                            </div>
+                            <div className="row profile-row" onClick={this.togglePopup.bind(this, "TimeZone")}>
+                                <div className="col-sm-3 alert-label-column">
+                                    <div className="form-group label-text">
+                                        <label className="control-label">Time Zone</label>
+                                    </div>
+                                </div>
+                                <div className="col-sm-9">
+                                    <p className="profile-label">
+                                        GMT +- ?
+                                    </p>
+                                    <i class="fas fa-edit edit-icon"></i>
+                                </div>
+                            </div>
+                            <div className="row">
+                                {
+                                    (loggedUserObj.permissions && loggedUserObj.permissions.canUpdate)
+                                        ? <div className="row">
+                                            <div className="col-sm-4 alert-label-column">
+                                            </div>
+                                            <div className="col-sm-3 alert-label-column">
+                                                <div className="form-group">
+                                                    <button
+                                                        className="btn btn-primary form-control button-all-caps-text" id="password-change"
+                                                        onClick={(e) => this.updateMailClick(e)}>
+                                                        Change Password
                                                 </button>
+                                                </div>
                                             </div>
                                         </div>
-                                     </div>
-                                    : null
-                            }
+                                        : null
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>
