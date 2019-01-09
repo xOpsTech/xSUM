d97899ddefd5703c44f0bd27e2859ebca8fe1590
Chinthaka Suren Fernando <mcsfernando10@gmail.com>
2019-01-09 13:57:22 +0530

    Seperate scheduler from service

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -3,13 +3,14 @@ var express = require('express'),
     apiRouter = require('./routes/router')
     config = require('./config/config');
 
+var Scheduler = require('./scheduler/scheduler');
+
 var port = config.PORT;
 var app = express();
-
-app.use('/assets', express.static('../assets'));
-app.use('/lib', express.static('../lib'));
-app.use('/sitespeed-result', express.static('./sitespeed-result'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', apiRouter);
 app.listen(port);
+
+// Start Scheduler
+Scheduler.startScheduler();
