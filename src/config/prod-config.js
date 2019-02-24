899368a1be536b539a5a90439d84e8e1bb0d18d5
Chinthaka Suren Fernando <mcsfernando10@gmail.com>
2019-02-24 14:25:28 +0530

    Add new locations for SERVER_LOCATION_ARRAY

--- src/config/prod-config.js
+++ src/config/prod-config.js
@@ -1,5 +1,8 @@
 export const API_URL = 'http://xsum.xops.it';
 export const SERVER_LOCATION_ARRAY = [
     {locationid: 0, textValue: 'USA', latitude: 36.778259, longitude: -119.417931},
-    {locationid: 1, textValue: 'UK', latitude: 51.509865, longitude: -0.118092}
+    {locationid: 1, textValue: 'UK', latitude: 51.509865, longitude: -0.118092},
+    {locationid: 2, textValue: 'Australia', latitude: -33.865143, longitude: 151.209900},
+    {locationid: 3, textValue: 'Singapore', latitude: 1.351616, longitude: 103.808053},
+    {locationid: 4, textValue: 'Japan', latitude: 35.652832, longitude: 139.839478}
 ];
