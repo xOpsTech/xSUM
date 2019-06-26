66a8aab1017838773bb041750147911dad5316fc
Chinthaka Suren Fernando <mcsfernando10@gmail.com>
2019-06-26 22:37:17 +0530

    Change influxdb server ip

--- backend/config/prod-config.js
+++ backend/config/prod-config.js
@@ -1,5 +1,5 @@
 exports.PORT = 5000;
-exports.INFLUXDB_IP = '10.142.0.15';
+exports.INFLUXDB_IP = '10.128.0.21';
 exports.MONGODB_IP = '10.142.0.4';
 exports.SERVER_LOCATION_ID = 0;
 exports.CLUSTER_URL = 'https://104.154.131.97';
